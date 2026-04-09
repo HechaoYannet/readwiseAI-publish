63a98189975b99a8804a79989f6d8fffa78e5a6d
UnSultan <yhcnet2021@163.com>
2026-04-09 17:24:05 +0800

    add files

--- readwiseAI-frontend/next.config.ts
+++ readwiseAI-frontend/next.config.ts
@@ -2,6 +2,12 @@ import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
   /* config options here */
+  output: 'export',  // 静态导出
+  distDir: 'dist',   // 输出目录
+  trailingSlash: true,
+  images: {
+    unoptimized: true,
+  },
 };
 
 export default nextConfig;
